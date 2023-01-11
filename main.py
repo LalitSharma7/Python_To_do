be5936a085b3420462ff3f8644c64d560e3fbdb3
Lalit <lalit1701sh@gmail.com>
2023-01-11 16:52:28 +0530

    Task for day 5 completed

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 
 todos = []
-
 while True:
-    user_action = input("Type add, show, edit, exit: ")
+    user_action = input("Type add, show, edit, complete or  exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
             todo = input("Enter todo: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index+1}-{item}"
+                print(row)
         case 'edit':
             number = int(input("Enter item number to edit: "))
             number = number-1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Enter todo to complete: "))
+            todos.pop(number-1)
         case 'exit':
             break
 
