ad20e03a6acdc6b42d6e8826318f620088c357b7
Sharma <lalisharma@deloitte.com>
2023-01-20 01:38:24 +0530

    Added GUI and add button

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
-def get_todos(filepath="todos.txt"):
+
+FilePath = "todos.txt"
+
+
+def get_todos(filepath=FilePath):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FilePath):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
